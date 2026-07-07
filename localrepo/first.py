2969dcf9fa9817d99d280ed51b42b140d890448f
ifrafatima12 <ifra4637@gmail.com>
2026-07-07 15:45:36 +0500

    adding first python repo

--- localrepo/first.py
+++ localrepo/first.py
@@ -1,1 +1,12 @@
-print (35-21)
+name = "ifra"
+age = 18
+price = 25.98
+print (name)
+print (age)
+print (price)
+print ("my name is:", name)
+age2 = age 
+print (age2)
+print (type(name))
+print (type(age))
+print (type(price))
