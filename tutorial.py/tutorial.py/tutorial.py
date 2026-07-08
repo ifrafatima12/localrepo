8764e75248bc7d26baa41f7541259e1d5e5c845a
ifrafatima12 <ifra4637@gmail.com>
2026-07-08 14:53:37 +0500

    first commit

---
+++ tutorial.py/tutorial.py/tutorial.py
@@ -0,0 +1,5 @@
+#taking input from user and printing it
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+price= float(input("Enter the price: "))
+print("Your name is:", name , "Your age is:", age , "The price is:", price) 
